6d121a8829f5731d0b947062a6dee8a6ec8c453c
Scott Schmit <i.grok@comcast.net>
2016-12-04 17:31:26 -0500

    day 3, part 2

--- 3/pgm.py
+++ 3/pgm.py
@@ -13,8 +13,14 @@ for line in sys.stdin.readlines():
     nums = map(int, line.strip().split())
     grid.append(nums)
 
+ctgrid = []
+for i in xrange(0, len(grid), 3):
+    ctgrid.append([grid[i][0], grid[i+1][0], grid[i+2][0]])
+    ctgrid.append([grid[i][1], grid[i+1][1], grid[i+2][1]])
+    ctgrid.append([grid[i][2], grid[i+1][2], grid[i+2][2]])
+
 n = 0
-for line in grid:
+for line in ctgrid:
     nums = line[:]
     if isTri(nums):
         n += 1
