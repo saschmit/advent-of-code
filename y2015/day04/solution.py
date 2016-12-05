63c89a7ea64d2d6932b72f1cb2a0dea96635eb25
Scott Schmit <i.grok@comcast.net>
2016-12-04 19:07:23 -0500

    day 4, part 2

--- y2015/day04/solution.py
+++ y2015/day04/solution.py
@@ -2,12 +2,12 @@
 
 from hashlib import md5
 
-def mine(key):
+def mine(key, count=5):
     n = 0
     digest = ""
-    while digest != "0" * 5:
+    while digest != "0" * count:
         n += 1
-        digest = md5(key + str(n)).hexdigest()[0:5]
+        digest = md5(key + str(n)).hexdigest()[0:count]
 
     return n
 
@@ -15,3 +15,4 @@ assert mine("abcdef") == 609043
 assert mine("pqrstuv") == 1048970
 
 print mine("yzbqklnj")
+print mine("yzbqklnj", 6)
