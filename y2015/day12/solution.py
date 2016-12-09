62fa60ddc4c04830d730abb382d5db28fa45bf1f
Scott Schmit <i.grok@comcast.net>
2016-12-09 01:14:24 -0500

    day 12, part 2

--- y2015/day12/solution.py
+++ y2015/day12/solution.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python
 
 import sys
+import json
+
+def total_it(j):
+    if type(j) is type(0):
+        return j
+
+    if type(j) is type({}):
+        if "red" not in j.values():
+            return sum(map(total_it, j.values()))
+
+    if type(j) is type([]):
+        return sum(map(total_it, j))
+
+    return 0
 
 for line in sys.stdin.readlines():
-    accum = ''
-    total = 0
-    sign = 1
-    for c in line:
-        if c in '0123456789':
-            accum += c
-        elif c == '-':
-            sign = -1
-        elif accum:
-            total += sign * int(accum)
-            accum = ''
-            sign = 1
-
-    print total
+    j = json.loads(line)
+    print total_it(j)
