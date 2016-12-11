9b6d476c836d42651b831cbf6b2f15abac81b927
Scott Schmit <i.grok@comcast.net>
2016-12-10 22:32:16 -0500

    solve day 17, part 2

--- y2015/day17/solution.py
+++ y2015/day17/solution.py
@@ -9,13 +9,19 @@ liters = int(sys.argv[1])
 for line in sys.stdin.readlines():
     containers.append(int(line.strip()))
 
-total = 0
+combinations = []
 for i in xrange(2**len(containers)):
     used = []
     for j in xrange(len(containers)):
         if i & (1 << j):
             used.append(containers[j])
     if sum(used) == liters:
-        total += 1
+        combinations.append(used)
 
-print total
+print len(combinations)
+
+least = min(map(len, combinations))
+
+print least
+
+print len(filter(lambda l: len(l) == least, combinations))
