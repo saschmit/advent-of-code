0dab4d3d865e579a680fe5bd22d959dc3c606400
Scott Schmit <i.grok@comcast.net>
2016-12-10 23:30:38 -0500

    solve day 19, part 1

---
+++ y2015/day19/solution.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import sys
+
+molecule = None
+xforms = []
+for line in sys.stdin.readlines():
+    tokens = line.strip().split(' ')
+    if len(tokens) == 1:
+        molecule = tokens[0]
+    elif len(tokens) == 0:
+        continue
+    elif len(tokens) == 3 and tokens[1] == '=>':
+        xforms.append(tokens[0::2])
+    else:
+        raise ValueError
+
+print molecule
+print xforms
+
+poss = set()
+for rule_in, rule_out in xforms:
+    n = molecule.find(rule_in)
+    while n != -1:
+        poss.add(molecule[:n] + rule_out + molecule[n+len(rule_in):])
+        n = molecule.find(rule_in, n+1)
+
+print len(poss)
