c6b3c11ea67ec1db1e1efde356c082ac728ee29f
Scott Schmit <i.grok@comcast.net>
2016-12-25 15:34:45 -0500

    day 13, this time with a correct solution

--- 13/map.py
+++ 13/map.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+from pprint import pprint
 
 favorite = 1364
 #favorite = 10
@@ -12,6 +13,7 @@ def iswall(x, y, fave=favorite):
     n = x*x + 3*x + 2*x*y + y + y*y + fave
     cursor = 1
     count = 0
+    # count bits in n
     while cursor <= n:
         count += int(bool(cursor & n))
         cursor = cursor << 1
@@ -23,7 +25,9 @@ def dist(a, b):
 
     return (ax - bx)**2 + (ay - by)**2
 
-def print_map(trail, current):
+def print_map(state):
+    trail = state.trail
+    current = state.pos
     X = max(map(lambda p: p[0], trail + [current]))
     Y = max(map(lambda p: p[1], trail + [current]))
 
@@ -42,53 +46,75 @@ def print_map(trail, current):
         print l
     print
 
+class State:
+    def __init__(self, pos, trail):
+        self.pos = tuple(pos)
+        self.trail = trail
+
+        self.x, self.y = self.pos
+
+    def __contains__(self, d):
+        return self.pos in d
+
+    def __str__(self):
+        return "(%d, %d; %d)" % (self.x, self.y, len(self.trail))
+    __repr__ = __str__
+
 target = (31, 39)
 #target = (7, 4)
-
-current = (1, 1)
 
 all_pos = {}
 
 trail = [ ]
 
-print current
-print_map(trail, current)
-while current != target:
-    candidates = []
-    x, y = current
-    if current in all_pos:
+candidates = [ State((1,1), []) ]
+
+current = candidates[0]
+
+winners = []
+
+while candidates:
+    candidates.sort(lambda a, b: cmp(dist(a.pos, target), dist(b.pos, target)) or cmp(len(a.trail), len(b.trail)))
+    #print "Candidates:"
+    #pprint(candidates)
+    #print
+    #print "Candidates distances: %s" % map(lambda q: dist(q, target), candidates)
+
+    current = candidates.pop(0)
+
+    print "currently at %s" % current
+    #print_map(current)
+
+    if current.pos == target:
+        print "found the target! len = %d" % len(current.trail)
+        winners.append(current)
+        continue
+
+    if current.pos in all_pos:
         print "I've been here before!"
+        if len(all_pos[current.pos].trail) > len(current.trail):
+            print "But this way is shorter"
+            all_pos[current.pos] = current
     else:
         print "Breaking new ground"
+        all_pos[current.pos] = current
 
     for offset in ((0, -1), (0, 1), (1, 0), (-1, 0)):
-        cand = (current[0] + offset[0], current[1] + offset[1])
+        trail = current.trail + [ current.pos ]
+        cand = (current.x + offset[0], current.y + offset[1])
         print "trying out %s" % (cand,)
         if iswall(cand[0], cand[1]):
             print "NO: %s is a wall" % (cand,)
             continue
-        elif cand in all_pos:
-            print "NO: %s has been seen before" % (cand,)
+        elif cand in all_pos and len(all_pos[cand].trail) <= len(current.trail):
+            print "NO: %s; %d has been seen before (or via a longer route)" % (cand, len(all_pos[cand].trail))
             continue
         else:
             print "MAYBE: %s queued" % (cand,)
-            candidates.append(cand)
+            candidates.append(State(cand, trail))
 
-    if candidates:
-        candidates.sort(lambda a, b: cmp(dist(a, target), dist(b, target)))
-        print "Candidates found: %s" % candidates
-        #print "Candidates distances: %s" % map(lambda q: dist(q, target), candidates)
-
-        nxt = candidates.pop(0)
-        all_pos[current] = candidates
-        trail.append(current)
-        current = nxt
-    else:
-        all_pos[current] = candidates
-        current = trail.pop()
-        print "Backtracking to %s" % (current, )
-
-    print current
-    print_map(trail, current)
-
-print len(trail)
+shortest_winner = min(map(lambda x: len(x.trail), winners))
+for winner in winners:
+    if len(winner.trail) == shortest_winner:
+        print len(winner.trail)
+        print_map(winner)
