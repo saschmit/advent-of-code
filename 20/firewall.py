2d635618b60a3f95cff72c75fccdd7cf4d22342a
Scott Schmit <i.grok@comcast.net>
2016-12-26 01:03:51 -0500

    day 20, optimized

--- 20/firewall.py
+++ 20/firewall.py
@@ -19,17 +19,30 @@ for line in sys.stdin.readlines():
 
 blocks.sort()
 
-for ip in xrange(2**32):
-    blocked = False
-    if ip % 2**16 == 0:
-        print ip
+print "raw: ", len(blocks)
 
-    for block in blocks:
-        if ip in block:
-            blocked = True
-            break
-    if not blocked:
-        print "unblocked: %d" % ip
-        exit(0) 
+old_len = None
+new_len = len(blocks)
+while old_len != new_len:
+    i = 0
+    while (i+1) < len(blocks):
+        if (blocks[i].end+1) >= blocks[i+1].start:
+            if blocks[i].end >= blocks[i+1].end:
+                del blocks[i+1]
+            else:
+                blocks[i] = Block("%d-%d" % (blocks[i].start, blocks[i+1].end))
+                del blocks[i+1]
+        else:
+            i += 1
 
-exit(1)
+    old_len = new_len
+    new_len = len(blocks)
+
+print "optimized: ", len(blocks)
+
+unblocked = blocks[0].start
+print "first unblocked = ", (blocks[0].end + 1)
+for i in xrange(1, len(blocks)):
+    unblocked += blocks[i].start - blocks[i-1].end - 1
+
+print "total unblocked = ", unblocked
