7a857cb014c79cceadc2cb36e930dbe3d1d12eb6
Scott Schmit <i.grok@comcast.net>
2018-12-24 02:48:38 -0600

    solve day 5 part 2

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -3,6 +3,20 @@ fn do_react(a : &u8, b : &u8) -> bool {
     (b.is_ascii_uppercase() && a.is_ascii_lowercase() && b.to_ascii_lowercase() == *a)
 }
 
+fn collapse(mut polymer : Vec<u8>) -> usize {
+    let mut i = 0;
+    while (i + 1) < polymer.len() {
+        if do_react(&polymer[i], &polymer[i+1]) {
+            polymer.remove(i+1);
+            polymer.remove(i);
+            i -= if i > 0 { 1 } else { 0 };
+        } else {
+            i += 1;
+        }
+    }
+    polymer.len()
+}
+
 fn main() {
     assert!(do_react(&b'a', &b'A'));
     assert!(do_react(&b'A', &b'a'));
@@ -21,15 +35,14 @@ fn main() {
     let mut buff = std::fs::read("input").unwrap();
     buff.retain(|&x| x.is_ascii_alphabetic());
 
-    let mut i = 0;
-    while (i + 1) < buff.len() {
-        if do_react(&buff[i], &buff[i+1]) {
-            buff.remove(i+1);
-            buff.remove(i);
-            i -= if i > 0 { 1 } else { 0 };
-        } else {
-            i += 1;
-        }
+    println!("Part 1: {}", collapse(buff.clone()));
+
+    let mut shortest = std::usize::MAX;
+    for unit in b'a'..=b'z' {
+        let mut polymer = buff.clone();
+        polymer.retain(|&x| x != unit && x != unit.to_ascii_uppercase());
+        let units = collapse(polymer);
+        shortest = std::cmp::min(shortest, units);
     }
-    println!("{}", buff.len());
+    println!("Part 2: {}", shortest);
 }
