2b117b81a4542a91f60a0eaabcb62a006fe81004
Scott Schmit <i.grok@comcast.net>
2019-01-25 16:44:38 -0500

    day 22 part 1

--- day22/src/tests.rs
+++ day22/src/tests.rs
@@ -4,11 +4,12 @@ use super::*;
 fn sample() {
     let depth = 510;
     let target = Pos { x: 10, y: 10 };
-    let survey = Survey::new(depth, target);
+    let mut survey = Survey::new(depth, target);
     assert_eq!(survey.scan[0][0], Region::Rocky);
     assert_eq!(survey.scan[0][1], Region::Wet);
     assert_eq!(survey.scan[1][0], Region::Rocky);
     assert_eq!(survey.scan[1][1], Region::Narrow);
     assert_eq!(survey.scan[10][10], Region::Rocky);
     assert_eq!(survey.get_risk_level(), 114);
+    assert_eq!(survey.find_minimum_time_path(), 45);
 }
