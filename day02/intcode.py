6c022d3ccdf7b69c17035e8439a2f0372d737f6d
Scott Schmit <i.grok@comcast.net>
2019-12-20 20:28:01 -0500

    solve day 5, part 1

--- day02/intcode.py
+++ day02/intcode.py
@@ -1,26 +1,46 @@
 #!/usr/bin/python
 
+from __future__ import division
 import sys
 
 def load_pgm(filename):
     return map(int, open(filename).read().strip().split(','))
 
-def run_pgm(program):
+POSITION_MODE = 0
+IMMEDIATE_MODE = 1
+
+def run_pgm(program, get_input=None, put_output=None):
     ip = 0
+
+    def param(param_num, write_val=None):
+        param_mode = program[ip] // 10**(2+param_num) % 10 
+        if param_mode == POSITION_MODE:
+            offset = program[ip + 1 + param_num]
+            if write_val is None:
+                return program[offset]
+            else:
+                program[offset] = write_val
+        elif param_mode == IMMEDIATE_MODE:
+            assert write_val is None
+            return program[ip + 1 + param_num]
+        else:
+            raise NotImplementedError("Illegal parameter mode")
+
+
     while True:
-        opcode = program[ip]
+        opcode = program[ip] % 100
         if opcode == 1:
-            src1 = program[ip + 1]
-            src2 = program[ip + 2]
-            dst = program[ip + 3]
-            program[dst] = program[src1] + program[src2]
+            param(2, param(0) + param(1))
             ip += 4
         elif opcode == 2:
-            src1 = program[ip + 1]
-            src2 = program[ip + 2]
-            dst = program[ip + 3]
-            program[dst] = program[src1] * program[src2]
+            param(2, param(0) * param(1))
             ip += 4
+        elif opcode == 3:
+            param(0, get_input())
+            ip += 2
+        elif opcode == 4:
+            put_output(param(0))
+            ip += 2
         elif opcode == 99:
             ip += 1
             break
