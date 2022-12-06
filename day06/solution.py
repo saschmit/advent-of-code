b14cb530283255fed64f73fb8293636bb7a7cc42
Scott Schmit <i.grok@comcast.net>
2022-12-06 00:08:12 -0500

    solve day 6, part 2

--- day06/solution.py
+++ day06/solution.py
@@ -7,5 +7,10 @@ for signal in open(sys.argv[1]):
     for n in range(3, len(signal)):
         candidate = set(list(signal[n-3:n+1]))
         if len(candidate) == 4:
-            print(n+1)
+            print("Part 1: {}".format(n+1))
+            break
+    for n in range(13, len(signal)):
+        candidate = set(list(signal[n-13:n+1]))
+        if len(candidate) == 14:
+            print("Part 2: {}".format(n+1))
             break
