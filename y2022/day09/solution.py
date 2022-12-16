15742776f5d00f9b7a3dc1509308f22838c16030
Scott Schmit <i.grok@comcast.net>
2022-12-16 00:25:56 -0500

    solve day 9, part 2

--- y2022/day09/solution.py
+++ y2022/day09/solution.py
@@ -2,8 +2,77 @@
 
 import sys
 
-debug = False
+def disp_grid(grid, start, rope):
+    for row in range(len(grid)):
+        out = ""
+        for col in range(len(grid[0])):
+            char = grid[row][col]
 
+            if row == start[0] and col == start[1]:
+                char = 's'
+
+            if rope is not None:
+                for knot in range(len(rope)-1, -1, -1):
+                    if row == rope[knot][0] and col == rope[knot][1]:
+                        char = str(knot)
+                if char == '0':
+                    char = 'H'
+                if len(rope) == 2 and char == '1':
+                    char = 'T'
+
+            out += char
+
+        print(out)
+
+def heal(head, tail):
+    def is_connected(hd, tl):
+        return abs(hd[0] - tl[0]) <= 1 and abs(hd[1] - tl[1]) <= 1
+
+    xlate = [
+    # col    +0        +1        +2        -2        -1
+        [ (+0, +0), (+0, +0), (+0, +1), (+0, -1), (+0, +0) ], # row + 0
+        [ (+0, +0), (+0, +0), (+1, +1), (+1, -1), (+0, +0) ], # row + 1
+        [ (+1, +0), (+1, +1), (+1, +1), (+1, -1), (+1, -1) ], # row + 2
+        [ (-1, +0), (-1, +1), (-1, +1), (-1, -1), (-1, -1) ], # row - 2
+        [ (+0, +0), (+0, +0), (-1, +1), (-1, -1), (+0, +0) ]  # row - 1
+    ]
+
+    delta = xlate[head[0] - tail[0]][head[1] - tail[1]]
+
+    new_tail = [None, None]
+    new_tail[0] = tail[0] + delta[0]
+    new_tail[1] = tail[1] + delta[1]
+
+    assert is_connected(head, new_tail)
+
+    return new_tail
+
+def xlate_rope(rope, dir):
+    new_rope = list(rope)
+    if dir == 'U':
+        new_rope[0][0] -= 1
+    elif dir == 'D':
+        new_rope[0][0] += 1
+    elif dir == 'L':
+        new_rope[0][1] -= 1
+    elif dir == 'R':
+        new_rope[0][1] += 1
+    else:
+        assert False
+
+    for knot in range(1, len(rope)):
+        new_rope[knot] = heal(new_rope[knot-1], new_rope[knot])
+
+    return new_rope
+
+debug = False
+if debug:
+    dbg_print = print
+else:
+    dbg_print = lambda x=None: None
+    disp_grid = lambda a, b, c: None
+
+# Load instructions
 inst = []
 for line in open(sys.argv[1]):
     dir, dist = line.strip().split()
@@ -28,98 +97,50 @@ for dir, dist in inst:
     else:
         assert False
 
-#print("l = {}, r = {}, u = {}, d = {}".format(lt, rt, up, dn))
+dbg_print("l = {}, r = {}, u = {}, d = {}".format(lt, rt, up, dn))
 width = rt - lt + 1
 height = dn - up + 1
-#print("{} x {}".format(height, width))
+dbg_print("{} x {}".format(height, width))
 
 # determine starting position
-start = [-up, -lt]
-#print(start)
+start = (-up, -lt)
+dbg_print(start)
 
-# Generate grid
-grid = [['.' for col in range(width)] for row in range(height)]
+def solve(height, width, start, rope_len):
+    # Generate grid
+    grid = [['.' for col in range(width)] for row in range(height)]
 
-# Mark start
-grid[start[0]][start[1]] = '#'
+    # Mark start
+    grid[start[0]][start[1]] = '#'
 
-head = list(start)
-tail = list(head)
+    rope = []
+    for knot in range(rope_len):
+        rope.append(list(start))
 
-def disp_grid(grid, start, head, tail):
-    for row in range(height):
-        out = ""
-        for col in range(width):
-            if head is not None and row == head[0] and col == head[1]:
-                out += 'H'
-            elif tail is not None and row == tail[0] and col == tail[1]:
-                out += 'T'
-            elif row == start[0] and col == start[1]:
-                out += 's'
-            else:
-                out += grid[row][col]
-        print(out)
-
-def xlate(head, tail, dir):
-    def is_connected(hd, tl):
-        return abs(hd[0] - tl[0]) <= 1 and abs(hd[1] - tl[1]) <= 1
-    assert is_connected(head, tail)
-
-    new_head = list(head)
-    new_tail = list(tail)
-
-    if dir == 'U':
-        new_head[0] = head[0] - 1
-        if not is_connected(new_head, tail):
-            new_tail[0] = tail[0] - 1
-            new_tail[1] = new_head[1]
-    elif dir == 'D':
-        new_head[0] = head[0] + 1
-        if not is_connected(new_head, tail):
-            new_tail[0] = tail[0] + 1
-            new_tail[1] = new_head[1]
-    elif dir == 'L':
-        new_head[1] = head[1] - 1
-        if not is_connected(new_head, tail):
-            new_tail[0] = new_head[0]
-            new_tail[1] = tail[1] - 1
-    elif dir == 'R':
-        new_head[1] = head[1] + 1
-        if not is_connected(new_head, tail):
-            new_tail[0] = new_head[0]
-            new_tail[1] = tail[1] + 1
-    else:
-        assert False
-
-    assert is_connected(new_head, new_tail)
-
-    return new_head, new_tail
-
-if debug:
-    dbg_print = print
-else:
-    dbg_print = lambda x=None: None
-    disp_grid = lambda a, b, c, d: None
-
-dbg_print("== Initial State ==")
-dbg_print()
-disp_grid(grid, start, head, tail)
-for dir, dist in inst:
+    dbg_print("== Initial State ==")
     dbg_print()
-    dbg_print("== {} {} ==".format(dir, dist))
-    for i in range(dist):
+    disp_grid(grid, start, rope)
+    for dir, dist in inst:
         dbg_print()
-        head, tail = xlate(head, tail, dir)
-        grid[tail[0]][tail[1]] = '#'
-        disp_grid(grid, start, head, tail)
+        dbg_print("== {} {} ==".format(dir, dist))
+        for i in range(dist):
+            dbg_print()
+            rope = xlate_rope(rope, dir)
+            grid[rope[-1][0]][rope[-1][1]] = '#'
+            disp_grid(grid, start, rope)
 
-dbg_print()
-dbg_print()
-disp_grid(grid, start, None, None)
+    dbg_print()
+    dbg_print()
+    disp_grid(grid, start, None)
 
-part1_count = 0
-for row in range(height):
-    for col in range(width):
-        part1_count += 1 if grid[row][col] == '#' else 0
+    count = 0
+    for row in range(height):
+        for col in range(width):
+            count += 1 if grid[row][col] == '#' else 0
+    return count
 
+part1_count = solve(height, width, start, 2)
 print("Part 1: {}".format(part1_count))
+
+part2_count = solve(height, width, start, 10)
+print("Part 2: {}".format(part2_count))
